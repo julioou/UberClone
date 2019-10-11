66bcc6470a58e72449b042e3fc34e7af0b1ac7ef
tes <tes>
2019-10-11 10:02:35 -0300

    Tela menu princiapl, tela de cadastro e login criado e arquivos controller criados.

---
+++ UberClone/Controller/CadastroViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CadastroViewController.swift
+//  UberClone
+//
+//  Created by Treinamento on 10/11/19.
+//  Copyright © 2019 JCAS. All rights reserved.
+//
+
+import UIKit
+
+class CadastroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UberClone/Controller/PrincipalViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UberClone
+//
+//  Created by Treinamento on 10/11/19.
+//  Copyright © 2019 JCAS. All rights reserved.
+//
+
+import UIKit
+
+class PrincipalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
